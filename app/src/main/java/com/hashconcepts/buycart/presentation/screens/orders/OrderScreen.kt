53e7aed36ccec3ffa9389fef2cdbae6b7054d55f
ifechukwu <ifechukwu.udorji@accelerexholdings.com>
2022-07-20 02:37:51 +0100

    working on orders

---
+++ app/src/main/java/com/hashconcepts/buycart/presentation/screens/orders/OrderScreen.kt
@@ -0,0 +1,19 @@
+package com.hashconcepts.buycart.presentation.screens.orders
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+/**
+ * @created 20/07/2022 - 2:37 AM
+ * @project BuyCart
+ * @author  ifechukwu.udorji
+ */
+
+@Destination
+@Composable
+fun OrderScreen(
+    navigator: DestinationsNavigator
+) {
+
+}
